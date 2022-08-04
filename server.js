a03974132670d20de2a040faca3279d02ac4beb3
Patrick <patrick.czaczka98@gmail.com>
2022-08-04 11:13:46 +1000

    added code that starts server

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ var express = require('express'); //used for routing
 var app = express();
 var http = require('http').Server(app); //used to provide http functionality
 app.use(express.static(__dirname + '/www'));
+
+let server = http.listen(3000, function(){
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("my first nodeJS server");
+    console.log("server listening on: " + host + "port: " + port);
+});
